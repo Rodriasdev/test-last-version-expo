d54ab489f4082f97b70a55ba69902953392fb861
Rodriasd <veronrodrigo98@gmail.com>
2024-05-20 15:27:02 -0300

    was added all the information of the game

--- app/(gameList)/gameInfo.tsx
+++ app/(gameList)/gameInfo.tsx
@@ -4,9 +4,9 @@ import { ScaledSheet } from "react-native-size-matters"
 import { useLocalSearchParams } from "expo-router"
 import { GameFindDto } from "@/types/GameFind.dto"
 const {width} = Dimensions.get('window')
+import { Link } from "expo-router"
+
 export default function GameInfo() {
-    // const route = useRoute<GameInfoRouteProp>()
-    // const {game}= route.params
 
     const {game} = useLocalSearchParams()
 
@@ -19,10 +19,38 @@ export default function GameInfo() {
         <View style={style.container}>
             <Image style={style.Image} source={{uri:objectGame.thumbnail}}/>
             <View style={style.InfoGame}>
-                <View>
                     <Text style={style.Text}>Nombre: </Text>
-                </View>
-                <View><Text>{objectGame.title}</Text></View>
+                    <Text style={style.DataGame}>{objectGame.title}</Text>
+            </View>
+            <View style={style.InfoGame}>
+                    <Text style={style.Text}>Descripción: </Text>
+                    <Text style={style.DataGame}>{objectGame.short_description}</Text>
+            </View>
+            <View style={style.InfoGame}>
+                    <Text style={style.Text}>Género: </Text>
+                    <Text style={style.DataGame}>{objectGame.genre}</Text>
+            </View>
+            <View style={style.InfoGame}>
+                    <Text style={style.Text}>Plataforma: </Text>
+                    <Text style={style.DataGame}>{objectGame.platform}</Text>
+            </View>
+            <View style={style.InfoGame}>
+                    <Text style={style.Text}>Distribuidores: </Text>
+                    <Text style={style.DataGame}>{objectGame.publisher}</Text>
+            </View>
+            <View style={style.InfoGame}>
+                    <Text style={style.Text}>Desarroladores: </Text>
+                    <Text style={style.DataGame}>{objectGame.developer}</Text>
+            </View>
+            <View style={style.InfoGame}>
+                    <Text style={style.Text}>Fecha de lanzamiento: </Text>
+                    <Text style={style.DataGame}>{objectGame.release_date}</Text>
+            </View>
+            <View>
+                    <Link style={style.link} href={objectGame.freetogame_profile_url}>Ver perfil en FreeToGame</Link>
+            </View>
+            <View>
+                    <Link style={style.link} href={objectGame.game_url}>Descargar el juego</Link>
             </View>
         </View>
     )
@@ -38,9 +66,24 @@ const style = ScaledSheet.create({
     },
     Text:{
         color: 'orange',
-        display: 'flex'
+        display: 'flex',
+        fontSize: 17
+    },
+    DataGame: {
+        fontSize: 17
     },
     InfoGame:{
-        display:'flex'
+        flexDirection:'row',
+        marginTop: 10,
+        marginHorizontal: 10,
+        width: width * 0.75,
+        marginVertical: 10
+    },
+    link: {
+        width: width,
+        textAlign: 'center',
+        fontSize: 20,
+        backgroundColor: 'orange',
+        marginVertical: 10
     }
 })
